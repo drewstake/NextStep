b5656d684a6376c89e894114ee8052a9639334a0
bzm436 <bzm436@psu.edu>
2025-04-07 23:38:23 -0400

    Retain apis list and config.js

---
+++ NextStep-mobile/config.js
@@ -0,0 +1,7 @@
+// API Configuration
+export const API_BASE_URL = 'http://localhost:4000';
+
+// Other configuration constants can be added here
+export const APP_NAME = 'NextStep';
+export const APP_VERSION = '1.0.0'; 
+export const GOOGLE_CLIENT_ID = '681971948277-sghsen822vlul0c98ffs8mqrnn4u8god.apps.googleusercontent.com';
