54d1b1e7c902e62586280b9ed3648af2727a31a6
bzm436 <bzm436@psu.edu>
2025-04-14 02:29:28 -0400

    fixed incorrect reference to config.js in server code

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,7 +4,6 @@ const { OAuth2Client } = require("google-auth-library");
 const { ObjectId } = require("mongodb");
 const crypto = require("crypto");
 const { sendEmail } = require("../middleware/mailer");
-const { API_SERVER } = require("../config");
 
 // Initialize Google OAuth client
 const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
